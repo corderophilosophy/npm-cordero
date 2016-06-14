7a520dc24fcb5d4c3c1dd6d8b5d799153b6fd151
Rich Cordero <rich@corderophilosophy.com>
2016-06-14 09:47:30 -0400

    write index.js :heart:

---
+++ README.md
@@ -0,0 +1,14 @@
+# cordero
+
+Like many others, I've made this to avoid someone else taking my name on npm.
+
+## Usage
+
+```javascript
+var cordero = require('cordero');
+console.log(cordero.contact.twitter); // 'https://twitter.com/corderophi678'
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: {
+    first: 'Richard',
+    last: 'Cordero'
+  },
+  contact: {
+    website: 'https://corderophilosophy.com',
+    twitter: 'https://twitter.com/corderophi678',
+    email: 'rich@corderophilosophy.com',
+    phone: 'available upon request',
+    github: 'https://github.com/corderophilosophy'
+  },
+  education: {
+    undergraduate: {
+      school: 'SUNY -- University at Buffalo',
+      major: 'Philosophy and Psychology',
+      degree: 'BA (2005)'
+    },
+    graduate: {
+      school: 'The Florida State University',
+      department: 'Philosophy',
+      degree: 'MA (2009), PhD (2015)',
+      dissertation: 'Particularism and Holism: Not a Necessary Marriage'
+    }
+  }
+}
